772d346b36101faa65aa65d9fc8cb12af888b1c4
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2025-12-14 21:15:20 +0700

    update and neat

--- script.js
+++ script.js
@@ -3,24 +3,26 @@ document.addEventListener("DOMContentLoaded", function() {
     let month = document.getElementById("month");
     let nameHbd = document.getElementById("name");
 
+    // save in local storage when input
     day.addEventListener("change", function() {
         localStorage.setItem("day",day.value)
     });
     month.addEventListener("change", function() {
         localStorage.setItem("month",month.value)
     });
-
     nameHbd.addEventListener("change", function() {
         localStorage.setItem("name",nameHbd.value);
         const title = document.querySelector("title");
         title.innerText=`Happy birthday ${localStorage.getItem("name")}`;
     });
 
+    // load name to title from local storage
     if(localStorage.getItem("name") !== null) {
         const title = document.querySelector("title");
         title.innerText=`Happy Birthday ${localStorage.getItem("name")}`;
     };
 
+    // validate input
     let closeNotif = document.getElementById("closeNotif");
     closeNotif.addEventListener("click", function() {
         if(day.value == "" || month.value == "" || nameHbd.value == "") {
@@ -32,11 +34,11 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
+    // show notification once time
     function alreadysee() {
         const notif = document.querySelector(".bgnotif");
         notif.className="closed";
     }
-
     function loadItem() {
         if(localStorage.getItem("notif") == "true") {
             alreadysee();
@@ -45,4 +47,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // initial load
     loadItem();
+
+    let main = document.createElement("main");
+    main.innerHTML=`
+    <h1>Happy Birthday, ${localStorage.getItem("name")}</h1>
+    `;
+    document.body.appendChild(main)
 });
