3701ebd523138661e193ef7dafcace67f519af51
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2025-12-14 12:56:04 +0700

    update

---
+++ script.js
@@ -0,0 +1,20 @@
+const day = document.getElementById("day");
+const month = document.getElementById("month");
+const nameHbd = document.getElementById("name");
+
+day.addEventListener("change", function() {
+    localStorage.setItem("day",day.value)
+})
+month.addEventListener("change", function() {
+    localStorage.setItem("month",month.value)
+})
+
+nameHbd.addEventListener("change", function() {
+    localStorage.setItem("name",nameHbd.value);
+})
+
+if(localStorage.getItem("name") !== null) {
+    const title = document.querySelector("title");
+    title.innerText=`Happy birthday ${localStorage.getItem("name")}`;
+    console.log("success");
+}
